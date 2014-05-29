a24f1e55681f5f2b5a0787d82cf0488d5b0c553b
sumitglobussoft <sumit@globussoft.com>
2014-05-29 13:06:24 +0530

    Updates & Bug Fixes 29-5-2014

---
+++ SocioBoard/Referral.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SocioBoard.Domain;
+
+namespace SocioBoard
+{
+    public partial class Referrals1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SocioBoard.Domain.User user = (User)Session["LoggedUser"];
+                if (user == null)
+                {
+
+                    Response.Redirect("Default.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.StackTrace);
+            }
+        }
+    }
+}
