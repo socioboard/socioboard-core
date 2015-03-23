b6f323f3e301887af9b16e69c87ae4744b166265
socioboard <sumit@socioboard.com>
2015-03-23 10:52:33 +0530

    Facebook & Linkedin Group Schedulers

---
+++ SocioBoard/SocioboardDataScheduler/FacebookGroupScheduler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocioboardDataScheduler
+{
+    class FacebookGroupScheduler:IScheduler
+    {
+
+        public string PostScheduleMessage(string scheduledmsgguid, string Userid, string profileid)
+        {
+            string str = string.Empty;
+            try
+            {
+                Api.Facebook.Facebook ApiobjFacebook = new Api.Facebook.Facebook();
+                str = ApiobjFacebook.SheduleFacebookGroupMessage(profileid, Userid, scheduledmsgguid);
+            }
+            catch (Exception ex)
+            {
+                str = ex.Message;
+            }
+            return str;
+        }
+
+
+    }
+}
---
+++ SocioBoard/SocioboardDataScheduler/LinkedinGroupScheduler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocioboardDataScheduler
+{
+    class LinkedinGroupScheduler:IScheduler
+    {
+        public string PostScheduleMessage(string scheduledmsgguid, string Userid, string profileid)
+        {
+            string str = string.Empty;
+            try
+            {
+                Api.Linkedin.Linkedin ApiObjLinkedin = new Api.Linkedin.Linkedin();
+                str = ApiObjLinkedin.ScheduleLinkedinGroupMessage(scheduledmsgguid,Userid,profileid);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            return str;
+        }
+    }
+}
